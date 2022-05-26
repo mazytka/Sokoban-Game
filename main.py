181d7bd289cf4af4da66046444028a989bd5732d
mazytka <95392543+mazytka@users.noreply.github.com>
2022-05-26 11:29:32 +0300

    Добавил границы окна

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pygame
 
 pygame.init()
 
-width = 800
-height = 640
+width = 10
+height = 10
 tile = 48
 fps = 30
 title = 'Sokoban game'
@@ -11,7 +11,7 @@ title = 'Sokoban game'
 color = (255, 0, 0)
 black = (0, 0, 0)
 
-window = pygame.display.set_mode((width, height))
+window = pygame.display.set_mode((width * tile, height * tile))
 pygame.display.set_caption(title)
 
 clock = pygame.time.Clock()
@@ -27,7 +27,7 @@ class Player:
         self.color = color
 
 
-player = Player(400, 400, 40, 40, color)
+player = Player(width * tile // 2, height * tile // 2, 40, 40, color)
 speed = player.width
 
 run = True
@@ -38,16 +38,16 @@ while run:
             run = False
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
+            if event.key == pygame.K_w and player.y > 0:
                 player.y -= speed
 
-            if event.key == pygame.K_s:
+            if event.key == pygame.K_s and player.y < width * tile - player.width:
                 player.y += speed
 
-            if event.key == pygame.K_d:
+            if event.key == pygame.K_d and player.x < height * tile - player.height:
                 player.x += speed
 
-            if event.key == pygame.K_a:
+            if event.key == pygame.K_a and player.x > 0:
                 player.x -= speed
 
     window.fill((0, 0, 0))
